c9b0e3ce3d6d71b333c176e3369e3733d9cc8ded
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-05-21 10:37:15 -0400

    1.0.3

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is a mod created for [SkyClient](https://github.com/nacrt/SkyblockClient). 
 Most of the code here was from my own work on [Skytils](https://github.com/Skytils/SkytilsMod)
 
 Additionally, this mod would not be possible without Open Source Software, namely code from Wynntils and ChatShortcuts
+
+If you choose to use any of my work, you must follow the terms of the GNU Affero General Public License v3.0, or any later version.
--- src/main/kotlin/mynameisjeff/skyblockclientupdater/SkyClientUpdater.kt
+++ src/main/kotlin/mynameisjeff/skyblockclientupdater/SkyClientUpdater.kt
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.gameevent.TickEvent
 @Mod(modid = "skyblockclientupdater", name = "SkyClient Updater", version = SkyClientUpdater.VERSION, clientSideOnly = true, modLanguage = "kotlin", modLanguageAdapter = "mynameisjeff.skyblockclientupdater.utils.kotlin.KotlinAdapter")
 object SkyClientUpdater {
 
-    const val VERSION = "1.0.2"
+    const val VERSION = "1.0.3"
 
     @JvmField
     val mc = Minecraft.getMinecraft()
