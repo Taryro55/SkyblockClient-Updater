fa7e14e6419f91ca2a7f9a261b21cfb4adda4a48
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2022-01-12 10:07:50 -0500

    new year new buildscript

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+        maven {
+            name = "sonatype"
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+        }
+        maven { url = uri("https://maven.minecraftforge.net/") }
+        maven { url = uri("https://jitpack.io") }
+    }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "net.minecraftforge.gradle.forge" -> useModule("com.github.Skytils:ForgeGradle:${requested.version}")
+            }
+        }
+    }
+}
+
+rootProject.name = "SkyblockClient-Updater"
